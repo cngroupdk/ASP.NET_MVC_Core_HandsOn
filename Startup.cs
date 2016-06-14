4dc96985745596113057ac40fa91dd81ef88276c
nohwnd <me@jakubjares.com>
2016-06-14 10:47:15 +0200

    Add Mvc and exception pages (inject IHostingEnvironment via build in injection container to determine if we should include DeveloperExceptionPages)

--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,35 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace HandsOn
 {
     public class Startup
     {
+        private IHostingEnvironment _environment;
+        public Startup(IHostingEnvironment environment)
+        { 
+            _environment = environment;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            if(_environment.IsDevelopment())
+                app.UseDeveloperExceptionPage(); 
+
+            app.UseMvc();
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
         }
