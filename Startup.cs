976132026d92a1c80cf06aa88ea0c0ebde73e105
nohwnd <me@jakubjares.com>
2016-06-14 10:32:43 +0200

    Add defalt files and static files middleware (serves our index.html file)

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace HandsOn
 {
@@ -20,6 +15,8 @@ namespace HandsOn
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
